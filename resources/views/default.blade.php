a49b8e2c1bfe00ac6a8b3f77ce054eb100a615a4
radix <radixalprino@gmail.com>
2023-06-22 21:04:47 +0700

    radixganteng

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,6 +4,6 @@
     <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3
     border">
         <div class="bi-house-fill me-3 fs-1"></div>
-        <h4 class="mb-0">sembarang {{ $pageTitle }}.</h4>
+        <h4 class="mb-0">santai {{ $pageTitle }}.</h4>
     </div>
 </div>
