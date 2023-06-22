18b041434d3b8f31ee898f9aec411a923c1315fb
Candelaria30 <ika.putri.candelaria@gmail.com>
2023-06-22 20:23:49 +0700

    tadi diganti

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,6 +4,6 @@
     <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3
     border">
         <div class="bi-house-fill me-3 fs-1"></div>
-        <h4 class="mb-0">deby {{ $pageTitle }}.</h4>
+        <h4 class="mb-0">ika {{ $pageTitle }}.</h4>
     </div>
 </div>
